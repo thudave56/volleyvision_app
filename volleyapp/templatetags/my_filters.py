ec292a1dbe38f00969b60fd62392ef09d091d3ab
Thrush23 <thudave56@gmail.com>
2025-02-13 14:47:28 -0500

    renamed directories

--- volleyapp/templatetags/my_filters.py
+++ volleyapp/templatetags/my_filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def extension(value):
     return value.split('.')[-1]
+
+@register.filter(name='add_class')
+def add_class(value, arg):
+    return value.as_widget(attrs={'class': arg})
